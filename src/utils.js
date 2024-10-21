f4303293f7fe8ab553388e7a36535e93c808ab45
SyedAyaanAli6786 <aliaayan6786@gmail.com>
2024-10-21 23:53:57 +0530

    connect deployed backend

--- src/utils.js
+++ src/utils.js
@@ -8,4 +8,4 @@ export const notify = (message, type) => {
     toast[type](message);
 }
 
-export const API_URL = 'http://localhost:8080';
+export const API_URL = 'https://task-manager-backend-spwi.onrender.com';
